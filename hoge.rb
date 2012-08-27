b12d09fa778004d2bae7011e3b4d04bda0807483
slightair <arksutite@gmail.com>
2012-08-27 09:11:24 +0900

    すこし整頓してみた

--- hoge.rb
+++ hoge.rb
@@ -1,19 +1,20 @@
 require 'faraday'
 require 'yaml'
+require 'json'
+
+require 'pp'
 
 config = YAML.load_file('./config.yaml')
 
-conn = Faraday::Connection.new(url: 'https://api.parse.com') do |builder|
+conn = Faraday::Connection.new('https://api.parse.com') do |builder|
   builder.request :url_encoded
   builder.adapter :net_http
-end
-
-response = conn.get do |request|
-  request.url '/1/classes/Foo'
-  request.headers = {
+  builder.headers = {
     'X-Parse-Application-Id' => config['parse']['app_id'],
     'X-Parse-REST-API-Key' => config['parse']['rest_api_key'],
   }
 end
 
-puts response.body
+response = conn.get '/1/classes/Foo'
+
+pp JSON.parse(response.body)
